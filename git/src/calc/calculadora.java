f0685a388a5b3d68c2b9f5cf0161ee0e28f18100
givamarques <49195218+givamarques@users.noreply.github.com>
2020-03-09 22:51:31 -0300

    arquivos add via upload

---
+++ git/src/calc/calculadora.java
@@ -0,0 +1,23 @@
+package calc;
+
+import java.util.Scanner;
+
+public class calculadora {
+public static void main(String[] args) {
+	
+	Scanner entrada = new Scanner (System.in);
+	
+	int numero1 = 0;
+	int numero2 = 0;
+	int soma = 0;
+	
+	System.out.print("Digite o primeiro numero: ");
+	numero1 = entrada.nextInt();
+	System.out.print("Digite o segundo numero: ");
+	numero2 = entrada.nextInt();
+	
+	soma = numero1+numero2;
+	
+	System.out.printf("A soma é %d\n", soma);
+}
+}
